956c5af390bd2d2c9ba3eb2775ad437ad93e30fe
Tëma Bolshakov <either.free@gmail.com>
2026-07-05 19:03:12 +0200

    Load .proquo.yml config in the Action and CLI entry points

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,5 +1,5 @@
 import {execFileSync} from "node:child_process";
-import {effectiveSize, price} from "@proquo/core";
+import {effectiveSize, loadConfig, price} from "@proquo/core";
 import {parseNumstat} from "./diff";
 import {renderReport} from "./report";
 
@@ -11,8 +11,9 @@ function gitNumstat(range: string | undefined): string {
 
 function main(): void {
     const range = process.argv[2];
+    const config = loadConfig(process.cwd());
     const files = parseNumstat(gitNumstat(range));
-    const size = effectiveSize(files);
+    const size = effectiveSize(files, config);
     process.stdout.write(renderReport(size, price(size.effectiveLines)) + "\n");
 }
 
